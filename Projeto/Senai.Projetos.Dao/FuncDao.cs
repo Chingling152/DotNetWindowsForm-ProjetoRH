1fdd6c2c57d72a7a69fcb8de565f5062ce934846
Franklin Bitencourt <Chingling152@hotmail.com>
2017-10-21 12:11:46 -0200

    Versão 0.0.7

---
+++ Projeto/Senai.Projetos.Dao/FuncDao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto.Senai.Projetos.Modelos;
+
+namespace Projeto.Senai.Projetos.Dao {
+    class FuncDao : IDao<Funcionario> {
+
+        public List<Funcionario> Consultar() {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(Funcionario t) {
+            throw new NotImplementedException();
+        }
+
+        public void Salvar(Funcionario t) {
+            throw new NotImplementedException();
+        }
+    }
+}
